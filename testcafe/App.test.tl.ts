dc5909c0bc98a493737ecc256ae9c90164d4456d
Oliver Zeigermann <oliver@zeigermann.de>
2020-12-14 18:19:12 +0100

    tests: e2e completed

--- testcafe/App.test.tl.ts
+++ testcafe/App.test.tl.ts
@@ -11,6 +11,9 @@ fixture `App Browser Test using Testing Library`
 const valueEl = screen.getByRole('presentation');
 const incrementBtn = screen.getByLabelText('Increment value');
 const decrementBtn = screen.getByLabelText('Decrement value');
+const amountInput = screen.getByLabelText('Set increment amount')
+const addAmountBtn = screen.getByLabelText('Add Amount (sync)');
+const addAmountBtnAsync = screen.getByLabelText('Add Amount (async)');
 
 test('increase', async t => {
 
@@ -22,6 +25,7 @@ test('increase', async t => {
 
 test('decrease', async t => {
     const initialValue = Number(await valueEl.innerText)
+
     await t.expect(initialValue).eql(0);
     await t.click(decrementBtn);
     const newValue = Number(await valueEl.innerText)
@@ -29,3 +33,39 @@ test('decrease', async t => {
 
 });
 
+// uses screenshot to understand what is going on
+// https://devexpress.github.io/testcafe/documentation/guides/advanced-guides/screenshots-and-videos.html
+test('add amount', async t => {
+    const increment = 9;
+    const initialValue = Number(await valueEl.innerText)
+
+    await t.takeElementScreenshot(valueEl);
+    await t.expect(initialValue).eql(0);
+    // this is needed in order not to add the 9, but replace existing text 
+    // await t.selectText(amountInput).pressKey("delete");
+    // or at least
+    await t.selectText(amountInput);
+    await t.typeText(amountInput, String(increment));
+    await t.click(addAmountBtn);
+    await t.takeElementScreenshot(valueEl);
+    const newValue = Number(await valueEl.innerText)
+    await t.expect(newValue).eql(initialValue + increment);
+
+    await t.takeScreenshot();
+});
+
+
+test('add amount async', async t => {
+    const increment = 9;
+    const initialValue = Number(await valueEl.innerText)
+
+    await t.expect(initialValue).eql(0);
+    await t.selectText(amountInput);
+    await t.typeText(amountInput, String(increment));
+    await t.click(addAmountBtnAsync);
+
+    await t.wait(1500);
+
+    const newValue = Number(await valueEl.innerText)
+    await t.expect(newValue).eql(initialValue + increment);
+});
