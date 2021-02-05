ac3d21d1f81b283d8b5a586ff993033fce850b67
Oliver Zeigermann <oliver@zeigermann.de>
2021-02-05 13:31:14 +0100

    tests: fixed for extended state

--- src/features/counter/counterSlice.test.ts
+++ src/features/counter/counterSlice.test.ts
@@ -6,14 +6,16 @@ import {
 
 test('reducer increments', () => {
   const {value} = counterReducer({
-    value: 0
+    value: 0,
+    operationInProgress: {}
   }, increment);
   expect(value).toBe(1);
 });
 
 test('reducer decrements', () => {
   const {value} = counterReducer({
-    value: 1
+    value: 1,
+    operationInProgress: {}
   }, decrement);
   expect(value).toBe(0);
 });
