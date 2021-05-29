b85744dc6b76ee5ddfc484b41313d2a1d6961256
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2021-05-29 13:14:41 +0200

    tests: action creators

--- src/features/counter/counterSlice.test.ts
+++ src/features/counter/counterSlice.test.ts
@@ -1,8 +1,25 @@
+import fetchMock from 'fetch-mock'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
 import counterReducer from './counterSlice';
 import {
   decrement,
-  increment
+  increment,
+  loadFromServer, 
+  selectCount,
+  startOperation,
+  endOperation,
+  setValue
 } from './counterSlice';
+import { store, RootState } from "../../app/store";
+
+const middlewares = [thunk]
+const mockStore = configureMockStore<RootState>(middlewares)
+
+afterEach(() => {
+  fetchMock.restore()
+});
 
 test('reducer increments', () => {
   const {value} = counterReducer({
@@ -20,3 +37,44 @@ test('reducer decrements', () => {
   expect(value).toBe(0);
 });
 
+// https://redux.js.org/recipes/writing-tests
+test('loaded data lands in state', async () => {
+  const newCount = 1000000; 
+  fetchMock.getOnce(process.env.PUBLIC_URL + "/api/users.json", {
+    body: {
+      count: newCount
+  },
+    headers: { 'content-type': 'application/json' }
+  });
+
+  await store.dispatch(loadFromServer())
+  const actualCount = selectCount(store.getState());
+  expect(actualCount).toBe(newCount);
+});
+
+test('loading dispatches start and end', async () => {
+  const newCount = 1000000; 
+  fetchMock.getOnce(process.env.PUBLIC_URL + "/api/users.json", {
+    body: {
+      count: newCount
+  },
+    headers: { 'content-type': 'application/json' }
+  });
+
+  // https://github.com/reduxjs/redux-mock-store
+  // mocking to check for actions
+  const mockedStore = mockStore(store.getState())
+  // type inference unsolved
+  // https://stackoverflow.com/questions/66570090/type-casting-dispatch-for-configuremockstore-from-createasyncthunk
+  const loadThunk: any = loadFromServer();
+  await mockedStore.dispatch(loadThunk)
+
+  const expectedActions = [
+    { type: startOperation.type, payload: 'LOAD'  },
+    { type: setValue.type, payload: newCount },
+    { type: endOperation.type, payload: 'LOAD' }
+  ]
+
+  // toBe will not work, not same object
+  expect(mockedStore.getActions()).toEqual(expectedActions);
+});
