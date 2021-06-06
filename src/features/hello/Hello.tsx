1eb010f6053fa00e9d0e0eb719a0624b7e3df96f
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2021-06-06 18:18:45 +0200

    example for styled components

--- src/features/hello/Hello.tsx
+++ src/features/hello/Hello.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
+import styled from 'styled-components';
+
+const Div = styled.div`
+  color: tomato;
+  border-color: tomato;
+  border-width: 0.1em;
+`;
+
 function Hello() {
   const { t } = useTranslation();
 
   return (
     <div id='hello-app' className="p-6 max-w-sm mx-auto space-x-5">
-      <p>
+      <Div>
         {t("hello")}
-      </p>
+      </Div>
     </div>
   );
 }
