03ded34d39290b0bb7329b038f91e9ac4c15bce9
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2021-06-07 13:52:48 +0200

    added basic web vitals

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { LoadingIndicator } from "./common/components/LoadingIndicator";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
+import {getCLS, getFID, getFCP, getLCP, getTTFB} from 'web-vitals';
 
 import "./i18n";
 import App from "./App";
@@ -26,3 +27,11 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// https://web.dev/vitals/
+
+getTTFB(console.log); // Time to First Byte (TTFB): https://web.dev/time-to-first-byte/, is the server slow?
+getFCP(console.log); // First Contentful Paint (FCP): https://web.dev/fcp/, first time user sees anything
+getLCP(console.log); // Largest Contentful Paint (LCP): https://web.dev/lcp/, first time user sees app
+getFID(console.log); // First Input Delay (FID): https://web.dev/fid/, once user can interact how long till app responds
+getCLS(console.log); // Cumulative Layout Shift (CLS): https://web.dev/cls/, is the app "jumpy"?
