6efb1fc771ba0bd1a75d5557797d5091484dd30d
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2021-06-10 12:16:18 +0200

    Updates

--- static/js/5.4cac36ed.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-showcase"]=this["webpackJsonpreact-showcase"]||[]).push([[5],{95:function(e,t,a){"use strict";a.r(t);var c,o=a(89),s=(a(0),a(88)),r=a(90),n=a(1),i=r.a.div(c||(c=Object(o.a)(["\n  color: tomato;\n  border-color: tomato;\n  border-width: 0.1em;\n"])));t.default=function(){var e=Object(s.a)().t;return Object(n.jsx)("div",{id:"hello-app",className:"p-6 max-w-sm mx-auto space-x-5",children:Object(n.jsx)(i,{children:e("hello")})})}}}]);
-//# sourceMappingURL=5.4cac36ed.chunk.js.map
